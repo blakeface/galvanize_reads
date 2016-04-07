18a9a774f9241c8f5f3db5e94323e5043c2bdc2b
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-07 12:29:01 -0600

    added morgan. added styling to / and /authors. created seeding file. created author.hbs - need to knex

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/authors', function(req, res, next) {
+  // knex('book_info')
+  res.render('authors');
 });
 
 module.exports = router;
