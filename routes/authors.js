6164a97842953b820075ad3c568d36842e3383bf
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-08 11:27:04 -0600

    on individual book / author page - now shows array of authors or books

--- routes/authors.js
+++ routes/authors.js
@@ -15,21 +15,29 @@ router.get('/', function(req, res, next) {
       result.push(author);
       return result;
     })
-
   }, []).then(function ( results ){
     res.render('authors', { authors: results })
   })
 })
 
-
 router.get('/:id', function(req, res, next) {
-  knex('authors')
-  .where({authors_id: req.params.id})
-  .innerJoin('books', 'authors.book_id', 'books.id')
-  .then(function ( results ){
-    res.render('authors', { authors: results });
+  knex('authors').where({id: req.params.id})
+  .reduce(function ( result, author ){
+    return knex('books')
+    .innerJoin('authors_books', 'books.id', 'authors_books.book_id')
+    .where({author_id: author.id})
+    .reduce(function ( books, book ){
+      books.push(book);
+      return books;
+    }, [] ).then(function ( books ){
+      author.books = books;
+      result.push(author);
+      return result;
+    })
+  }, []).then(function ( results ){
+    res.render('authors', { authors: results })
   })
-});
+})
 
 router.get('/:id/edit', function(req, res, next) {
   knex('authors')
--- routes/books.js
+++ routes/books.js
@@ -22,13 +22,24 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/:id', function(req, res, next) {
-  knex('books')
-  .where({id: req.params.id})
-  .innerJoin('authors', 'books.id', 'authors.book_id')
-  .then(function ( results ){
-    res.render('books', { books: results });
+  knex('books').where({id: req.params.id})
+  .reduce(function ( result, book ){
+    return knex('authors')
+    .innerJoin('authors_books', 'authors.id', 'authors_books.author_id')
+    .where({book_id: book.id})
+    .reduce(function ( authors, author ){
+      authors.push(author);
+      return authors;
+    }, [] ).then(function ( authors ){
+      book.authors = authors;
+      result.push(book);
+      return result;
+    })
+
+  }, []).then(function ( results ){
+    res.render('books', { books: results })
   })
-});
+})
 
 router.get('/:id/edit', function(req, res, next) {
   knex('books')
