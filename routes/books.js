ea7dcefce30592a0d118cf9756e6104125313864
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-08 11:58:59 -0600

    added styling to edit form. both edit forms support numerous authors / books

--- routes/books.js
+++ routes/books.js
@@ -35,17 +35,26 @@ router.get('/:id', function(req, res, next) {
       result.push(book);
       return result;
     })
-
-  }, []).then(function ( results ){
+  }, [] ).then(function ( results ){
     res.render('books', { books: results })
   })
 })
 
 router.get('/:id/edit', function(req, res, next) {
-  knex('books')
-  .where({id: req.params.id})
-  .innerJoin('authors', 'books.id', 'authors.book_id')
-  .then(function ( results ){
+  knex('books').where({id: req.params.id})
+  .reduce(function ( result, book ){
+    return knex('authors')
+    .innerJoin('authors_books', 'authors.id', 'authors_books.author_id')
+    .where({book_id: book.id})
+    .reduce(function ( authors, author ){
+      authors.push(author);
+      return authors;
+    }, [] ).then(function ( authors ){
+      book.authors = authors;
+      result.push(book);
+      return result;
+    })
+  }, [] ).then(function ( results ){
     res.render('edit-book', { book: results });
   })
 });
